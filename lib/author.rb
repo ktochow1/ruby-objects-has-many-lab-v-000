c9ecc04085a8b80e63471f338d5d5267ee4f127c
ktochow1 <ktochow1@binghamton.edu>
2018-11-19 18:46:04 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -18,7 +18,7 @@ class Author
   def add_post_by_title(name)
     new_post = Post.new(name)
     add_post(new_post)
-    new_post(Post.new(name)) = self
+    new_post(Post.new(name)) = self.name
     
   end
     
