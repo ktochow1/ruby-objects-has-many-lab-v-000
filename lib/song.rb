a83d933057d26032c92fe5ebd23a7c0d7a9787b1
ktochow1 <ktochow1@binghamton.edu>
2018-11-19 21:49:43 +0000

    automatically backed up by learn

--- lib/song.rb
+++ lib/song.rb
@@ -5,24 +5,11 @@ class Song
   
    @@all = []
    
-  # def name=(song_name)
-  #   @song_name = song_name
-  # end
-  
-  # def name 
-  #   @song_name
-  # end
-   
   def initialize(name)
     @name = name
     @@all << self
   end
-  
-  # def add_song_by_name(song_name)
-  #   new_song = Song.new(song_name)
-  #   add_song(new_song)
-  # end
-  
+
   def self.all 
     @@all
   end
