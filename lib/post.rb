a6e29d17b84b58b1d3a58417bf84d3fa4ede47aa
ktochow1 <ktochow1@binghamton.edu>
2018-11-19 21:09:47 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -8,12 +8,12 @@ class Post
   def initialize(title)
     @title = title
     @@all = []
-    # @@all << self.new(title)
+    @@all << self.new(title)
     @@all
   end 
   
  def add_post(a_post)
-   @@all << self.new(title)
+   
     @posts.push(a_post)
     a_post.author = self
     @@post_counter += 1
